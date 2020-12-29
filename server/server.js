99f14522e0555983e036546001067312879cad0e
aosorio <aosorio9559@gmail.com>
2020-12-28 23:41:52 -0500

    Se configuran peticiones

--- server/server.js
+++ server/server.js
@@ -1,34 +1,20 @@
 require("./config/config");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(require("./routes/usuario"))
 
-app.get("/usuario", (req, res) => {
-  res.json("Heloo");
+
+mongoose.connect("mongodb://localhost:27017/cafe", (error, res) => {
+  if (error) throw error
+  console.log("BD en línea");
 });
 
-app.post("/usuario", (req, res) => {
-  const body = req.body;
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: "Nombre es necesario",
-    });
-  } else {
-    res.json({ persona: body });
-  }
-});
 
-app.put("/usuario/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ id });
-});
-
-app.delete("/usuario", (req, res) => {
-  res.json("Heloo");
-});
-
-app.listen(process.env.PORT, () => console.log("Listening on port", process.env.PORT));
+app.listen(process.env.PORT, () =>
+  console.log("Listening on port", process.env.PORT)
+);
