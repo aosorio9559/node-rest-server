0c3c7a1fa96e3dc6959d6de141ade004ecf513c3
alejoosoriodiaz <tilolucho@gmail.com>
2020-12-30 00:51:37 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -9,9 +9,7 @@ let urlDb;
 if (process.env.NODE_ENV === "dev") {
   urlDb = "mongodb://localhost:27017/cafe";
 } else {
-  urlDb =
-    "mongodb+srv://aosorio:yB7rUsvThyKPdtda@cluster0.j3rbi.mongodb.net/cafe";
+  urlDb = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDb;
-
